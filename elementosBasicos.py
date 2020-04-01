bea854bf5f21bfc65eaca40daec5005a084596a0
alan <alanarostegui@hotmail.com>
2020-03-31 23:47:17 -0600

    elementos basicos de python

---
+++ elementosBasicos.py
@@ -0,0 +1,20 @@
+print(1+2)
+print(2 * 'hola')
+print('Hola mundo')
+print('Alan' + 'Maranto')
+print(2.0 * 3)
+print(2 ** 3)
+print(2 % 3)
+print(2 / 3)
+
+print(1 + 'hola') # error
+
+my_int = 1
+my_float = 1.0
+my_bool = False
+my_none = None
+
+print(type(my_int))
+print(type(my_float))
+print(type(my_bool))
+print(type(my_none))
