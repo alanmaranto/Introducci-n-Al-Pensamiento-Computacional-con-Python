7a180347911a7f4d3f5967d8fb373abf00477157
alan <alanarostegui@hotmail.com>
2020-05-19 22:26:53 -0500

    cadenas y entradas

---
+++ cadenas.py
@@ -0,0 +1,18 @@
+print('123')
+print('123' * 3)
+print('123' + '456')
+print(('hip' * 3) + ' '+ 'hurra')
+print(f' {"Hip " * 3} hurra')
+
+my_str = 'Alan'
+len(my_str)
+my_str[0]
+my_str[2:]
+my_str[:3]
+my_str[:-2]
+my_str[::2]
+
+# Concatenar 
+'Yo amo a ' + my_str
+f'Yo amo a {my_str}'
+f'Yo amo a {my_str}, ' * 100
---
+++ inputs.py
@@ -0,0 +1,11 @@
+nombre = input('Cual es tu nombre: ')
+print(nombre)
+print('Tu nombre es ', nombre)
+print(f'Tu nombre es {nombre}')
+
+numero = input('Escribe un numero: ')
+print(numero)
+print(type(numero))
+
+# Convertir str a number
+numero = int(input('Escribe un numero: '))
