10f6cda136707bf7aa6b61bdf3a6fa79bbd6db99
alan <alanarostegui@hotmail.com>
2020-07-25 16:38:34 -0500

    iteraciontes

---
+++ iteraciones.py
@@ -0,0 +1,14 @@
+""" extern_counter = 0
+intern_counter = 0
+
+while extern_counter < 5:
+    while intern_counter < 6:
+        print(extern_counter, intern_counter)
+        intern_counter += 1
+
+        if intern_counter >= 3:
+            break
+    extern_counter += 1
+    intern_counter = 0
+ """
+
